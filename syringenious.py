9a1e35b9b0f8e60b505546b331a0d7474593aff4
Bart Weil <43183347+Bart-Weil@users.noreply.github.com>
2023-07-24 02:48:16 +0100

    Update and rename gen.py to syringenious.py

--- syringenious.py
+++ syringenious.py
@@ -4,53 +4,38 @@ import english_words
 
 alph = list(string.ascii_lowercase)
 
+# Length of syllable "tiles"
 k = 4
 
 n_data = []
 
 for i in english_words.english_words_lower_alpha_set:
-
 	if len(i) >= max(8,k):
-
 		n_data.append(i)
 
 secs = []
 
 for i in n_data:
-
 	for j in range(len(i)-k):
-
 		secs.append(i[j:(j+k+1)])
 
 secs = list(set(secs))
-
 ends = list(set([i[-k:] for i in n_data]))
 
 def generate(n):
-
+	#attempts to generate words of length n or over
 	word = random.choice(n_data)[:k]
-
 	while True:
-
 		compat = []
-
 		for i in secs:
-
 			if i[0:k-1] == word[-k+1:]:
-
 				compat.append(i)
-
 		if len(compat) == 0:
-
 			print("fail")
 			return word
-
 		word += random.choice(compat)[k-1]
-
 		if word[-k:] in ends and len(word)>=n:
-
 			return word
 
 for i in range(100):
 	print(generate(5))
-
